33269f83d5b9e71db8b1944e788ce3c0bf63316f
Alex Declino <44327037+AlexDeclino@users.noreply.github.com>
2026-02-08 19:58:27 +0100

    content added

--- entries.js
+++ entries.js
@@ -1,19 +1,27 @@
 var ENTRIES_DATA = [
   {
     "type": "text",
-    "content": "SETTING:\n- Slightly war thorn Europe\n- A character name Sybil\n- The machinery controlling him\n- The device connecting the twos"
+    "content": "SETTING:\nThe year is 2029\nEurope is ridden by the rubble of war, its map resembles one from 1444 AD.\nA conjuction of sovereign cities, privately owned.\nThe unemployment rate is 99.7%\nYou receive your 700 EU tokens universal basic income from the \"government\"\nthe entire economy is ai data centers and gambling your life savings on real life tragedies and events\nYou have 3 days to gamble it on a prediction market to make your 2,800/mo rent on a Palantir owned studio apartment in the sovereign city of New Neukolln.\nIf you can’t quadruple the money, the government seizes your clothes and send you to be on Season 9 of Beast Games \nThe loser of the season is hunted alive by the elites for sport, live streamed on TikTok.\nThe data is used to train new biological intelligent drones for war."
   },
   {
     "type": "image",
-    "src": "content/1.jpg"
-  },
-  {
-    "type": "image",
-    "src": "content/2.jpg"
+    "src": "content/eumap.png"
   },
   {
     "type": "image",
     "src": "content/3.jpg"
+  },
+  {
+    "type": "keyword",
+    "content": "YOU ARE SYBIL."
+  },
+  {
+    "type": "keyword",
+    "content": "YOU ARE IN DEBT."
+  },
+  {
+    "type": "keyword",
+    "content": "You must go on. You can't go on. You will go on."
   },
   {
     "type": "video",
@@ -24,23 +32,53 @@ var ENTRIES_DATA = [
     "src": "content/sybil2.mp4"
   },
   {
+    "type": "text",
+    "content": "We follow SYBIL on his (pronouns tbd) daily trip to survive in this environment."
+  },
+  {
     "type": "link",
     "title": "IAN CHENG'S BIKEY",
-    "description": "literary style I would use",
+    "description": "literary style I would like to use",
     "url": "https://www.ribbonfarm.com/2019/06/18/worlding-raga-6-world-to-live/"
-  },
-  {
-    "type": "link",
-    "title": "THE POACHER",
-    "description": "Something",
-    "url": "https://hypergambling.declino.systems/content/poacher.pdf"
-  },
-  {
-    "type": "text",
-    "content": "IMA SCROBBLER HONEY. WE SCROBBLE ALBUMS ELECTRONICALLTY SO THE\nNUMBERS GO UP"
   },
   {
     "type": "image",
     "src": "content/map.png"
+  },
+  {
+    "type": "text",
+    "content": "In a scene he meets someone and define his job like:\n\nIMA SCROBBLER HONEY. \nWE SCROBBLE ALBUMS ELECTRONICALLTY SO THE NUMBERS GO UP"
+  },
+  {
+    "type": "image",
+    "src": "content/cage.jpeg"
+  },
+  {
+    "type": "image",
+    "src": "content/quote.png"
+  },
+  {
+    "type": "text",
+    "content": "It is important, to my mind, that he has only a poor peasant’s tools—no sword, no fine machete, no chainsaw. With clumsy tools, he almost has to chew his way through the awful thorn hedge—so slowly that it keeps growing back, so it takes him ages. Just the opposite of the Prince, whose sword whips the way through at a touch. The Prince of course is the “right one,” the Chosen. The peasant boy is a transgressor, the wrong one, the one with no right to succeed. It cannot be easy for him. Only intense passion and obsessive patience can bring him through.  Sort of like the practice of an art, yes. Passion, patience, obsession."
+  },
+  {
+    "type": "text",
+    "content": "prediction markets will replace buying stuff. \ni want someone to bring kiwis to my house.\ni make a prediction market about whether someone will deliver 4 kiwis to my doorstep and load $15 into \"no\". a guy with an ebike sees it and picks up some kiwis.\nbefore dropping them off on my doorstep, he bets yes. he drops them off. the market resolves to \"yes\" and he gets $15.\nrest in peace, amazon, doordash, ubereats, etc."
+  },
+  {
+    "type": "keyword",
+    "content": "YOU HAVE A SIMPLE DEVICE."
+  },
+  {
+    "type": "keyword",
+    "content": "A POTLACH BETWEEN THE INDIVIDUAL AND THE SYSTEM"
+  },
+  {
+    "type": "video",
+    "src": "content/device.mp4"
+  },
+  {
+    "type": "text",
+    "content": "Dice & Wagers (Mesopotamia, 3000 BCE) Playing Cards (China, 900 CE) Stock Exchanges (Amsterdam, 1602) Slot Machines (USA, 1894) Derivatives Trading (Chicago, 1973) Online Poker (1990s) Crypto Exchanges (2015) Money Console (DEGN, 2025)"
   }
 ];
