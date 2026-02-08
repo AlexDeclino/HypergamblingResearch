e51201ae7709ce031ba6f604b90306056823675d
Alex Declino <44327037+AlexDeclino@users.noreply.github.com>
2026-02-08 16:57:48 +0100

    Initial commit

---
+++ entries.js
@@ -0,0 +1,18 @@
+var ENTRIES_DATA = [
+  {
+    "type": "text",
+    "content": "askjdhasdkasjdh"
+  },
+  {
+    "type": "image",
+    "src": "content/img.png"
+  },
+  {
+    "type": "text",
+    "content": "aaaaaaaaa"
+  },
+  {
+    "type": "video",
+    "src": "content/vid.mp4"
+  }
+];
