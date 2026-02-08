e76e124b4b2c598c788dc4eefda7c513e61e63cb
Alex Declino <44327037+AlexDeclino@users.noreply.github.com>
2026-02-08 17:22:55 +0100

    content adjusted

--- entries.js
+++ entries.js
@@ -1,18 +1,26 @@
 var ENTRIES_DATA = [
   {
     "type": "text",
-    "content": "askjdhasdkasjdh"
+    "content": "SETTING:\n- Slightly war thorn Europe\n- A character name Sybil\n- The machinery controlling him\n- The device connecting the twos"
   },
   {
     "type": "image",
-    "src": "content/img.png"
+    "src": "content/1.jpg"
   },
   {
-    "type": "text",
-    "content": "aaaaaaaaa"
+    "type": "image",
+    "src": "content/2.jpg"
+  },
+  {
+    "type": "image",
+    "src": "content/3.jpg"
   },
   {
     "type": "video",
-    "src": "content/vid.mp4"
+    "src": "content/sybil1.mp4"
+  },
+  {
+    "type": "video",
+    "src": "content/sybil2.mp4"
   }
 ];
